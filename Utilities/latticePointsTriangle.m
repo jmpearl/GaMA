8ebf31558241dc7a4b3ce9e8e46a7ec503d0aa40
Jason Pearl <jasonmpearl91@gmail.com>
2021-12-01 23:20:20 -0800

    cleaning up our quad-node generation for tri

--- Utilities/latticePointsTriangle.m
+++ Utilities/latticePointsTriangle.m
@@ -1,16 +1,24 @@
 function [u,v] = latticePointsTriangle(d)
-    
-    numPts = (d+2)*(d+1)/2;
-    
-    u = zeros(numPts,1);
-    v = zeros(numPts,1);
-    
-    i1 = 1;
-    for i = 0:d
-        i2 = i1+d-i;
-        u(i1:i2) =  i/d*ones(1,d-i+1);
-        v(i1:i2) = linspace(d-i,0,d-i+1)/d;
-        i1 = i2+1;
+ 
+    % centroid model for d=0
+    if d == 0
+        u = 1/3;
+        v = 1/3;
+
+    % all other degrees
+    else
+        numPts = (d+2)*(d+1)/2;
+
+        u = zeros(numPts,1);
+        v = zeros(numPts,1);
+
+        i1 = 1;
+        for i = 0:d
+            i2 = i1+d-i;
+            u(i1:i2) =  i/d*ones(1,d-i+1);
+            v(i1:i2) = linspace(d-i,0,d-i+1)/d;
+            i1 = i2+1;
+        end
     end
     
 end

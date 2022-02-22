546604ae6a8f55c2a557468f3ab29fc00730d4fb
Jason Pearl <jasonmpearl91@gmail.com>
2022-02-21 23:55:59 -0800

    octave compatibility

--- src/Mesh/createLattice.m
+++ src/Mesh/createLattice.m
@@ -22,7 +22,7 @@ function [coordinates] = createLattice(ds,minExtent,maxExtent,latticeType)
     assert(length(maxExtent)==3,"max extent must be a 1x3")
     assert(length(minExtent)==3,"min extent must be a 1x3")
     assert(all(maxExtent>minExtent),"min extent must be smaller than max")
-    assert(any(strcmp(latticeType,["pc","fcc","bcc"])),"lattice type must be: simple, fcc, or bcc") %#ok<STCI> 
+    assert(any(strcmp(latticeType,{"pc","fcc","bcc"})),"lattice type must be: pc, fcc, or bcc") %#ok<STCI> 
     
     % Number of elements per dimension
     numStepsx = round((maxExtent(1)-minExtent(1))/ds);
